c58b991acb30811d7979cb7e1e59bf2729cb9454
tarunkumar-ra <90757528+tarunkumar-ra@users.noreply.github.com>
2022-04-08 18:37:37 +0530

    COMMIT

---
+++ src/component/home.js
@@ -0,0 +1,97 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div>Home</div>
+      <p>
+        e 2022 Indian Premier League, also known as IPL 15 or, for sponsorship
+        reasons, Tata IPL 2022,[2] is the fifteenth season of the Indian Premier
+        League (IPL), a professional Twenty20 cricket league established by the
+        Board of Control for Cricket in India (BCCI) in 2007. The tournament is
+        being played from 26 March 2022, and to be concluded with the final on
+        29 May 2022. The group stage of the tournament is being played entirely
+        in the state of Maharashtra, with Mumbai and Pune hosting the
+        matches.[3] The full schedule of the tournament was announced on 6 March
+        2022.[4] The season saw the expansion of the league, with the addition
+        of two new franchises.[5][6] Therefore, this was the second season to
+        have ten teams, after the 2011 tournament.[7] Chennai Super Kings are
+        the defending champions, having won their fourth title during the
+        previous season.[8] Contents 1 Background 2 Personnel changes 3 Format 4
+        Venues 5 Teams and standings 5.1 Points table 5.2 Match summary 6 Group
+        stage 6.1 Matches 7 Playoffs 8 References 9 External links Background
+        Although earlier reports suggested the addition of two more teams in the
+        previous season,[9][10][11] the BCCI at their 89th AGM announced that
+        the expansion of the league would occur only in 2022.[12][13] In August
+        2021, the BCCI confirmed that two new franchises would join the league
+        starting from the 2022 season. It was also announced that the franchises
+        would be based in two of the six cities shortlisted by the BCCI;
+        Ahmedabad, Cuttack, Dharamshala, Guwahati, Indore and Lucknow.[14][15]
+        In a closed bidding held on 25 October 2021, RPSG Group and CVC Capital
+        won bids for the two teams.[16][17] RPSG paid ₹7,090 crore (US$930
+        million) for Lucknow, whereas CVC won Ahmedabad for ₹5,625 crore (US$740
+        million).[18][19] The Lucknow team was named as Lucknow Super Giants on
+        24 January 2022,[20] whereas the Ahmedabad team was named as Gujarat
+        Titans on 9 February 2022.[21] Vivo pulled out as the title sponsor of
+        the tournament on 11 January 2022. Vivo, having previously pulled out as
+        sponsors in 2020, had signed contract as title sponsors till 2023. The
+        Tata Group was named as the title sponsor for the remainder of Vivo's
+        contract.[22][23] Personnel changes Main article: List of 2022 Indian
+        Premier League personnel changes Each existing team was allowed to
+        retain a maximum of four players, whereas the two new teams were allowed
+        to pick a maximum of three players before the auction.[24] The retained
+        players of the existing eight teams were announced on 30 November
+        2021,[25][26] and the two new teams named their players on 22 January
+        2022.[27][28] The auction took place on 12 and 13 February 2022 in
+        Bangalore.[29] Ishan Kishan was the most expensive buy of the auction,
+        bought by Mumbai Indians for ₹15.25 crore (US$2.0 million).[30] The most
+        expensive foreign player was Liam Livingstone, bought by Punjab Kings
+        for ₹11.50 crore (US$1.5 million).[31] Format With the introduction of
+        the new teams, a ten-team format was created. This format consists of 74
+        matches and was introduced as retaining the previous format would result
+        in 94 matches, significantly greater than the 60 matches from the
+        previous season, where teams compete in a double round-robin tournament.
+        The ten teams are divided into two groups of five. In the group stage,
+        each team plays 14 games: facing the other four teams in their group two
+        times each (one home and one away game), four teams in the other group
+        once, and the remaining team two times. The groups for the tournament
+        were announced on 25 February 2022.[32] Each team plays the team in the
+        same row and the same column twice, and all others once. For instance,
+        Mumbai Indians will play Chennai Super Kings and the other Group A teams
+        twice but the other teams from Group B (Sunrisers Hyderabad, Royal
+        Challengers Bangalore, Punjab Kings and Gujarat Titans) only once.
+        Similarly, Chennai will play Mumbai and the other Group B teams twice
+        but all other teams from Group A only once. The format used is similar
+        to the one used in 2011, with the only difference being that the teams
+        were drawn according to seedings instead of being drawn randomly.[33]
+        The figures in parenthesis denote the number of titles won by the team.
+        Group A Group B Mumbai Indians (5) Chennai Super Kings (4) Kolkata
+        Knight Riders (2) Sunrisers Hyderabad (1) Rajasthan Royals (1) Royal
+        Challengers Bangalore Delhi Capitals Punjab Kings Lucknow Super Giants
+        Gujarat Titans Venues Three venues in Mumbai and one in Pune will host
+        the league stage matches. The Wankhede Stadium in Mumbai and the DY
+        Patil Stadium in Navi Mumbai will host 20 matches each, whereas Mumbai's
+        Brabourne Stadium and the MCA International Stadium in Pune will host 15
+        matches each. Every team will play four matches each at the Wankhede and
+        the DY Patil Stadium, and three matches each at the Brabourne and the
+        MCA Stadium.[34] 2022 Indian Premier League is located in
+        IndiaMumbaiMumbaiPunePuneNavi MumbaiNavi Mumbai Mumbai Navi Mumbai Pune
+        Wankhede Stadium Brabourne Stadium DY Patil Stadium MCA International
+        Stadium Capacity: 33,108 Capacity: 20,000 Capacity: 55,000 Capacity:
+        37,000 Wankhede ICC WCF.jpg Brabourne.jpg D Y Patil Sports Stadium.jpg
+        Sahara Stadium Pune 1.jpg Teams and standings Points table Pos Grp
+        Teamvte Pld W L NR Pts NRR Qualification 1 A Kolkata Knight Riders 4 3 1
+        0 6 1.102 Advance to Qualifier 1 2 A Lucknow Super Giants 4 3 1 0 6
+        0.256 3 A Rajasthan Royals 3 2 1 0 4 1.218 Advance to Eliminator 4 B
+        Gujarat Titans 2 2 0 0 4 0.495 5 B Punjab Kings 3 2 1 0 4 0.238 6 B
+        Royal Challengers Bangalore 3 2 1 0 4 0.159 7 A Delhi Capitals 3 1 2 0 2
+        −0.116 8 B Chennai Super Kings 3 0 3 0 0 −1.251 9 A Mumbai Indians 3 0 3
+        0 0 −1.362 10 B Sunrisers Hyderabad 2 0 2 0 0 −1.825 Updated to
+        match(es) played on 7 April 2022. Source: IPLT20.com The four top-ranked
+        teams qualify for the playoffs.
+      </p>
+    </>
+  );
+};
+
+export default Home;
